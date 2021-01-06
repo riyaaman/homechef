e8dd52cc5469bc52e53398836dc18eaa8ee75cf4
riyaaman <74763446+riyaaman@users.noreply.github.com>
2021-01-06 21:13:21 +0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,13 @@ module.exports.connect  =   function (done){
     var url           =    process.env.MONGODB_URL;
     const dbname      =   'db-snacky'
     
-    mongoClient.connect(url,(err,data)=>{
+    mongoClient.connect(url|| 'mongodb://localhost:27017',{ useNewUrlParser: true },(err,data)=>{
         if(err) return done(err)
         // if (err) throw err
         state.db = data.db(dbname)
         done()
     })
-  
-}
+    
 
 module.exports.get= function(){
     return state.db
