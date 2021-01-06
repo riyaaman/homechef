86dde8627ffb9b72450b3f0ebb9c294c5263e9db
riyaaman <74763446+riyaaman@users.noreply.github.com>
2021-01-06 16:13:30 +0400

    Update app.js

--- app.js
+++ app.js
@@ -10,18 +10,9 @@ var vendorRouter      =   require('./routes/vendor');
 
 var hbs               =   require('express-handlebars');
 var app               =   express();
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log("Our app is running ");
-// });
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Our app is running ");
 });
 
 
