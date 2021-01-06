e52939a966deea6b77a0e3d4e31d9a68af45a05f
riyaaman <74763446+riyaaman@users.noreply.github.com>
2021-01-06 14:02:32 +0400

    mongodb cluster connection added

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ const mongoClient       =   require('mongodb').MongoClient
 const state             =   {db:null}
 module.exports.connect  =   function (done){
     
-    const url       =   'mongodb://localhost:27017'
+    //const url       =   'mongodb://localhost:27017'
+     const url       =   'mongodb+srv://riyarahim:ashriya@cluster0.jvvz7.mongodb.net/test'
     const dbname    =   'db-snacky'
    
     mongoClient.connect(url,(err,data)=>{
