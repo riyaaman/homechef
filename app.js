9e007c533d39b7a7c1a25270b6c2a1496547cb03
riyaaman <74763446+riyaaman@users.noreply.github.com>
2021-01-06 16:10:14 +0400

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,22 @@ var vendorRouter      =   require('./routes/vendor');
 
 var hbs               =   require('express-handlebars');
 var app               =   express();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Our app is running ");
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log("Our app is running ");
+// });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
+
+
 var db                =   require('./config/connection');
 var session           =   require('express-session');
 var fileUpload        =   require('express-fileupload')
