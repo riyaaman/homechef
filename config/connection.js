6b5598c7fff8f1abb1d591770810739df4d030ed
riyaaman <riyaaman111@gmail.com>
2021-01-07 15:56:24 +0400

    app.js changed

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const mongoClient       =   require('mongodb').MongoClient
 const state             =   {db:null}
 module.exports.connect  =   function (done){
     
-   const url       =   'mongodb://localhost:27017'
+  // const url       =   'mongodb://localhost:27017'
    // const url       =   'mongodb+srv://riyarahim:ashriya@cluster0.jvvz7.mongodb.net/test'
-    var url           =    process.env.MONGODB_URI;
+   var url           =    process.env.MONGODB_URI;
     const dbname      =   'db-snacky'
     
     mongoClient.connect(url,(err,data)=>{
