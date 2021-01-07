add3923fb54b40b48b73dedeb758575f487fd7ff
riyaaman <74763446+riyaaman@users.noreply.github.com>
2021-01-07 09:08:57 +0400

    Update app.js

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use(function(err, req, res, next) {
   
   // res.render('error', {title: "Sorry,Something Went Wrong"});
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log("Our app is running ");
 });
